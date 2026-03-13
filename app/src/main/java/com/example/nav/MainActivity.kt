70630517da606b618a8753d680a4bef856d0c53b
Pablo Lambert <pablomartin.lambert@gmail.com>
2026-03-12 22:01:53 -0300

    Toolbar

--- app/src/main/java/com/example/nav/MainActivity.kt
+++ app/src/main/java/com/example/nav/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.nav
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
@@ -14,7 +17,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = MainActivityBinding.inflate(layoutInflater)
-        //setSupportActionBar(binding.toolbar)
+        setSupportActionBar(binding.toolbar)
         setupNavHost()
         setContentView(binding.root)
     }
@@ -26,15 +29,31 @@ class MainActivity : AppCompatActivity() {
         navController = navHostFragment.navController
         setupActionBarWithNavController(navController)
     }
-    //configurar el actionbar
-//    private fun configurateActionBar(title: String,displayHomeAsUpEnabled: Boolean) {
-//        supportActionBar?.title = title
-//        supportActionBar?.setDisplayHomeAsUpEnabled(displayHomeAsUpEnabled)
-//        supportActionBar?.setDisplayShowHomeEnabled(displayHomeAsUpEnabled)
-//
-//    }
+
 
     override fun onSupportNavigateUp(): Boolean {
         return navController.navigateUp() || super.onSupportNavigateUp()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.toolbar, menu)
+        //todo add funtions search
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.actions_search->{
+                //todo actions button
+                true
+            }
+            R.id.actions_settings->{
+                //implementar toast toast
+                Toast.makeText(this, "Settings", Toast.LENGTH_SHORT).show()
+
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
